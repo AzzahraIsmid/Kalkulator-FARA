800659f3261303e18d5e3b051569051b2b717b82
AzzahraIsmid <62925003+AzzahraIsmid@users.noreply.github.com>
2020-04-04 13:33:49 +0700

    menambah option perkalian

--- kalkulator.php
+++ kalkulator.php
@@ -1,47 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kalkulator</title>
-    <link rel="stylesheet" type="text/css" href="edit.css">
-   </head>
-   <body>
-    <?php
-    if(isset($_POST['hitung'])){
-      $bil1 = $_POST['bil1'];
-      $bil2 = $_POST['bil2'];
-      $operasi = $_POST['operasi'];
-      switch ($operasi) {
-        case 'tambah';
-          $hasil = $bil1+$bil2;
-        break;
-        case 'bagi';
-          $hasil = $bil1/$bil2;
-        break;
-        case 'kurang';
-              $hasil = $bil1-$bil2;
-        break;
-      }
-     }
-     ?>
-          <div class="kalkulator">
-        <h2 class="judul">===KALKULATOR FARA===</h2>
-        
-        <form method="post">
-            <input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama">
-            <input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
-            <select clas="opt" name="operasi">
-              <option value="tambah">+</option>
-                <option value="bagi">/</option>
-                <option value="kurang">-</option>
-              </select>
-            <td><input type="submit" name="hitung" value="Hitung" class="tombol"></td>
-            <td><input type="reset" name="reset" value="C" class="tombol"></td>
-          </form>
-          <?php if(isset($_POST['hitung'])){ ?>
-            <input type="text" value="<?php echo $hasil; ?>" class="bil">
-          <?php }else { ?>
-            <input type="text" value="0" class="bil">
-          <?php } ?>
-         </div>
-        </body>
-       </html>
+	<title>Kalkulator Sederhana Dengan PHP </title>
+	<link rel="stylesheet" type="text/css" href="edit.css">
+</head>
+<body>
+	<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+	<div class="kalkulator">
+		<h2 class="judul">====KALKULATOR====</h2>
+	
+		<form method="post">			
+			<input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama">
+			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<td><input type="submit" name="hitung" value="Hitung" class="tombol"></td>
+			<td><input type="reset" name="reset" value="C" class="tombol"></td>									
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>			
+	</div>
+</body>
+</html>
